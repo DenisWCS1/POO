9616daad1a5ed742ada0df2c43366fdbdc520d2e
PERRISSEAU Denis (SNCF RESEAU / INFRAPOLE PAYS DE LA LOIRE / Equipe Spécialiste Inform) <Denis>
2023-01-12 11:43:04 +0100

    Class creates

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+const Fighter = require("./src/Fighter.js");
+
+const Fighters = [
+  new Fighter("🧔", "Heracles", 20, 6),
+  new Fighter("🦁", "Nemean Lion", 11, 13),
+];
+console.log(Fighters[0].fight(Fighters[1]));
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,24 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+class Fighter {
+  constructor(icone, name, strength, dexterity, life) {
+    this.icone = icone;
+    this.name = name;
+    this.strength = strength;
+    this.dexterity = dexterity;
+    this.life = MAX_LIFE;
+  }
+  fight(defender) {
+    const attPoints = this.getRandomInt(this.strength);
+    const impact = attPoints - defender.dexterity;
+    defender.life = Math.max(defender.life - impact, 0);
+  }
+  getRandomInt(max) {
+    return 1 + Math.floor(Math.random() * max);
+  }
+  Resultat() {
+    return this.life > 0;
+  }
+}
+
+module.exports = Fighter;
